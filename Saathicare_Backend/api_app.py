dd3fc1fdf84e242ee26219dd9dc4bb13ffd0c64b
Ankush Pandey <ap539813@gmail.com>
2024-03-21 12:35:40 +0530

    Update api_app.py

--- Saathicare_Backend/api_app.py
+++ Saathicare_Backend/api_app.py
@@ -1,3 +1,17 @@
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+import os
+
+import json
+
+
+with open('api_keys.json') as api_file:
+    api_keys = json.load(api_file)
+    os.environ["MISTRAL_API_KEY"] = api_keys['mistral_api']
+
+
+
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'caresaathi_servicefile.json'
 
@@ -15,22 +29,44 @@ ENDPOINT_ID="6002025068819906560"
 PROJECT_ID="1093938084624"
 
 
-def predict_vertex_ai(endpoint_id, project_id, instance, location="us-central1"):
-    # client_options = {"api_endpoint": f"us-central1-aiplatform.googleapis.com"}
-    # client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+def predict_vertex_ai(endpoint_id, project_id, instance, context, tag, location="us-central1"):
+    if len(context) < 15 and tag == 'report':
+        api_key = os.environ["MISTRAL_API_KEY"]
+        model = "open-mistral-7b"
 
-    # endpoint = client.endpoint_path(
-    #     project=project_id,
-    #     location=location,
-    #     endpoint=endpoint_id
-    # )
+        client = MistralClient(api_key=api_key)
 
-    # response = client.predict(
-    #     endpoint=endpoint,
-    #     instances=[instance]
-    # )
+        messages = [
+            ChatMessage(role="user", content=instance["prompt"])
+        ]
 
-    return ['response.predictions']
+        chat_response = client.chat(
+            model=model,
+            messages=messages,
+        )
+        return [chat_response.choices[0].message.content]
+        
+    else:
+        client_options = {"api_endpoint": f"us-central1-aiplatform.googleapis.com"}
+        client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+
+        endpoint = client.endpoint_path(
+            project=project_id,
+            location=location,
+            endpoint=endpoint_id
+        )
+
+        response = client.predict(
+            endpoint=endpoint,
+            instances=[instance]
+        )
+
+        print(response.predictions)
+        return response.predictions
+
+def valid_response(prediction):
+    print(prediction)
+    return prediction.strip() == '' or ('[' in prediction and ']' in prediction) or 'Prompt:' in prediction
 
 
 @app.route('/health', methods=['GET'])
@@ -44,6 +80,7 @@ def interactive_physician_chatbot():
     print(data)
     question = data['question']
     tag = data['tag']
+    context = data['context']
 
     if tag == 'symptom':
         instance = {"prompt": question,
@@ -51,28 +88,41 @@ def interactive_physician_chatbot():
                 "temperature": 1.0,
                 "top_p": 1.0,
                 "top_k": 10}
-        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance)[0].replace('*', '').split('Symptom:')[-1].split('\n')[0].strip()
+        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].replace('*', '').split('Symptom:')[-1].split('\n')[0].strip()
+        while valid_response(prediction):
+            prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].replace('*', '').split('Symptom:')[-1].split('\n')[0].strip()
+        
+    
     elif tag == 'lifestyle':
         instance = {"prompt": question,
                 "max_tokens": 512,
                 "temperature": 1.0,
                 "top_p": 1.0,
                 "top_k": 10}
-        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance)[0].replace('*', '').split('Lifestyle:')[-1].split('\n')[0].strip()
+        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].replace('*', '').split('Lifestyle:')[-1].split('\n')[0].strip()
+        while valid_response(prediction):
+            prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].replace('*', '').split('Lifestyle:')[-1].split('\n')[0].strip()
+        
     elif tag == 'genetic':
         instance = {"prompt": question,
                 "max_tokens": 512,
                 "temperature": 1.0,
                 "top_p": 1.0,
                 "top_k": 10}
-        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance)[0].replace('*', '').split('Genetic:')[-1].split('\n')[0].strip()
+        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].replace('*', '').split('Genetic:')[-1].split('\n')[0].strip()
+        while valid_response(prediction):
+            prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].replace('*', '').split('Genetic:')[-1].split('\n')[0].strip()
+        
     elif tag == 'report':
         instance = {"prompt": question,
                 "max_tokens": 512,
                 "temperature": 1.0,
                 "top_p": 1.0,
                 "top_k": 10}
-        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance)[0].split('END OF RESPONSE\nOutput:')[-1].strip().replace('*', '').replace('END OF RESPONSE', '').replace('"', '')
+        prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].split('END OF RESPONSE\nOutput:')[-1].strip().replace('*', '').replace('END OF RESPONSE', '').replace('"', '')
+        while valid_response(prediction):
+            prediction = predict_vertex_ai(ENDPOINT_ID, PROJECT_ID, instance, context, tag)[0].split('END OF RESPONSE\nOutput:')[-1].strip().replace('*', '').replace('END OF RESPONSE', '').replace('"', '')
+        
     else:
         return jsonify({"error": "Invalid tag"}), 400
 
