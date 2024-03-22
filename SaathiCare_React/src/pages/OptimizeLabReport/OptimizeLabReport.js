290fa6a6f607f614a534ce5162cffe5de914790f
Ritu Saini <saini.rt21@gmail.com>
2024-03-22 13:57:12 +0530

    NavBar Update

---
+++ SaathiCare_React/src/pages/OptimizeLabReport/OptimizeLabReport.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './OptimizeLabReport.css';
+
+const OptimizeLabReport = () => {
+  return <div>Optimize Lab Report Page</div>;
+};
+
+export default OptimizeLabReport;
