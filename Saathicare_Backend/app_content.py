9f520f948d8f2b729b4c3c5977f568bba7dbd4a8
Ankush Pandey <ap539813@gmail.com>
2024-03-24 13:42:19 +0530

    Update app_content.py

--- Saathicare_Backend/app_content.py
+++ Saathicare_Backend/app_content.py
@@ -66,4 +66,4 @@ def projected_cases():
     return ci_df.reset_index().to_json(orient='records')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9090)
+    app.run(host='0.0.0.0', debug=True, port=9090)
